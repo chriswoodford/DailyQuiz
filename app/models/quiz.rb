d63642823a7d7d673ec2f3871054480bfe4779c2
Chris Woodford <chris.woodford@gmail.com>
2012-11-12 15:54:44 -0500

    update quiz results display

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,15 @@ class Quiz < ActiveRecord::Base
   belongs_to :player
   belongs_to :quiz_definition
   has_many :answers
+  
+  def correct_answers()
+    answers = [];
+    self.answers.each do |answer|
+      if answer.is_correct?
+        answers << answer
+      end
+    end
+    return answers
+  end
+  
 end
