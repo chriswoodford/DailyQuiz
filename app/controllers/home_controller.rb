27cf47358acc0821a11ff98cc2d4d5a7d12535b8
Chris Woodford <chris.woodford@gmail.com>
2013-02-18 13:42:17 -0500

    separate home page from user index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,18 @@
 class HomeController < ApplicationController
 
   def index
-    @daily_quiz = current_quiz
-    @teams = Team.all
+
+    if not signed_in?
+      @daily_quiz = current_quiz
+      @teams = Team.all
+      return render 'home'
+    end
+
+    @user = current_user
+
+  end
+
+  def home
   end
 
 end
