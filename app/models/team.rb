ad0e8cc122388b6178436dd5935c9b3d7d9b0a35
Chris Woodford <chris.woodford@gmail.com>
2013-02-17 23:06:53 -0500

    clean up team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -13,19 +13,14 @@ class Team < ActiveRecord::Base
   has_many :members, through: :team_members, source: :player
   attr_accessible :name
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  
+
   def has_played_today?
-    
-    memo = false
     members.reduce {|memo, player| memo ||= player.has_played_today? }
-    
   end
-  
+
   def points
-    
     0
     #members.reduce {|count, player| count += player.points(quiz) }
-    
   end
-  
+
 end
